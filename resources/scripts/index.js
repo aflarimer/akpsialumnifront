f0b585fb68e05c96dfafef4f4e3833649046670e
Anne Larimer <annelarimer@Annes-MacBook-Pro.local>
2021-11-11 14:06:07 -0600

    classupdates

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,5 +1,7 @@
+//const baseUrl = "https://akpsi-alumnidatabase.herokuapp.com/api/person";
+//const baseUrlLocal = "https://localhost:5001/api/person";
+
 function handleOnLoad() {
-    //const peopleUrl = "https://localhost:5001/api/person"
     const peopleUrl = "https://akpsi-alumnidatabase.herokuapp.com/api/person";
     fetch(peopleUrl).then(function(response){
         return response.json();
@@ -14,10 +16,52 @@ function handleOnLoad() {
 
 function displayTable(json){
     var dataTable = document.getElementById("dataTable");
-    var html = "<table><tr><th>First Name</th><th>Last Name</th><th>City</th></tr>";
+    var html = "<table class=\"table table-striped table-light\"><tr><th>First Name</th><th>Last Name</th><th>City</th></tr>";
     json.forEach(person => {
         html+=`<tr><td>${person.firstName}</td><td>${person.lastName}</td><td>${person.city}</td></tr>`;
     });
     html+="</table>";
     dataTable.innerHTML = html;
+}
+
+function handleOnSubmit() {
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lName").value;
+    var major = document.getElementById("major").value;
+    var city = document.getElementById("city").value;
+    var placeholder = "Not Provided";
+
+    var person = {
+        FirstName: fName,
+        LastName: lName,
+        Major: major,
+        Minor: placeholder,
+        PledgeClass: placeholder,
+        GraduatingSemester: placeholder,
+        GradSchool: placeholder,
+        GradSchoolName: placeholder,
+        Employed: placeholder,
+        Position: placeholder,
+        Company: placeholder,
+        City: city,
+        Email: placeholder
+    }
+    postPerson(person);
+}
+
+function postPerson(person) {
+        const personUrl = "https://akpsi-alumnidatabase.herokuapp.com/api/person";
+        fetch(personUrl,{
+            method: "POST",
+            headers: {
+                "Accept":'application/json',
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(person)
+        })
+        .then((response)=>{
+            console.log(response);
+            handleOnLoad();
+        });
+    
 }
